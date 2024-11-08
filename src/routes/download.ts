08e1ce91dd6a89a3f237aac9601010d56949f5fa
OrangeGoo <gjc20021013@gmail.com>
2024-11-07 21:53:04 -0500

    fix: error handler

--- src/routes/download.ts
+++ src/routes/download.ts
@@ -1,29 +1,29 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const downloadRoutes = new Hono();
 
 downloadRoutes.get("/:id/:cookie", async (c) => {
-  try {
-    const { id, cookie } = c.req.param();
+  const { id, cookie } = c.req.param();
 
-    const response = await fetch(
-      `${process.env.API_URL}/song/download/url?id=${id}&cookie=${cookie}`,
-      {
-        method: "GET",
-      }
-    );
-
-    if (!response.ok) {
-      console.error("Fetch error:", response.statusText);
-      return c.json({ error: "Failed to download song" }, 500);
+  const response = await fetch(
+    `${process.env.API_URL}/song/download/url?id=${id}&cookie=${cookie}`,
+    {
+      method: "GET",
     }
+  );
 
-    const data = await response.json();
-    return c.json(data);
-  } catch (error) {
-    console.error("Error processing request:", error);
-    return c.json({ error: "Internal Server Error" }, 500);
+  if (!response.ok) {
+    throw new HTTPException(500, { message: "Failed to download song" });
   }
+
+  const data = await response.json();
+  return c.json(data);
+});
+
+downloadRoutes.onError((error, c) => {
+  console.error("Error processing request:", error);
+  return c.json({ error: error.message || "Internal Server Error" }, 500);
 });
 
 export default downloadRoutes;
--- src/routes/playlist.ts
+++ src/routes/playlist.ts
@@ -1,31 +1,30 @@
 import { Hono } from "hono";
-
+import { HTTPException } from "hono/http-exception";
 const playlistRoutes = new Hono();
 
 playlistRoutes.post("/", async (c) => {
-  try {
-    const { keywords } = await c.req.json();
+  const { keywords } = await c.req.json();
 
-    const response = await fetch(
-      `${process.env.API_URL}/cloudsearch?keywords=${encodeURIComponent(
-        keywords
-      )}`,
-      {
-        method: "POST",
-      }
-    );
-
-    if (!response.ok) {
-      console.error("Fetch error:", response.statusText);
-      return c.json({ error: "Failed to fetch playlist" }, 500);
+  const response = await fetch(
+    `${process.env.API_URL}/cloudsearch?keywords=${encodeURIComponent(
+      keywords
+    )}`,
+    {
+      method: "POST",
     }
+  );
 
-    const data = await response.json();
-    return c.json(data);
-  } catch (error) {
-    console.error("Error processing request:", error);
-    return c.json({ error: "Internal Server Error" }, 500);
+  if (!response.ok) {
+    throw new HTTPException(500, { message: "Failed to fetch playlist" });
   }
+
+  const data = await response.json();
+  return c.json(data);
+});
+
+playlistRoutes.onError((error, c) => {
+  console.error("Error processing request:", error);
+  return c.json({ error: error.message || "Internal Server Error" }, 500);
 });
 
 export default playlistRoutes;
